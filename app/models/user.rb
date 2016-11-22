fc1a06ad79e6d5ddd3fce9a0f3035896767f0f2b
Marcel Galang <marcelgalang@gmail.com>
2016-11-22 00:11:40 +0000

    Login/out addressed, stubbed out add user to list, Oauth still issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable
-  devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,17 @@ Rails.application.routes.draw do
       resources :items
     end
 
+  root "lists#index"
+
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
 
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
-  get '/login' => 'sessions#new'
-  post '/sessions' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
 
-  root 'lists#index'
+
+
+
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
